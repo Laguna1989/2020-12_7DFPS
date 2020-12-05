46e0cc8d0c6c931e9ba2a9d9af42be69f557ac2b
Simon Weis <laguna.ws@gmail.com>
2020-12-05 07:55:56 +0100

    Fix player walking direction due to left handed coordinate system

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,10 +12,13 @@ void Player::doUpdate(float elapsed)
     if (jt::InputManager::pressed(jt::KeyCode::A)) {
         angle += elapsed * GP::PlayerTurningSpeed();
     }
-
+    jt::vector2 const dir
+        = { cos(jt::MathHelper::deg2rad(angle)), -sin(jt::MathHelper::deg2rad(angle)) };
     if (jt::InputManager::pressed(jt::KeyCode::W)) {
-        jt::vector2 const dir
-            = { cos(jt::MathHelper::deg2rad(angle)), sin(jt::MathHelper::deg2rad(angle)) };
         position = position + elapsed * 1.0f * dir;
     }
+    if (jt::InputManager::pressed(jt::KeyCode::S)) {
+        position = position + elapsed * -1.0f * dir;
+    }
+    std::cout << position.x() << " " << position.y() << "\n";
 }
